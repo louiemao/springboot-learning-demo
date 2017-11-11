8e3af97ed97903340c1d82ff0f7c2c5319368e81
Louie <258362034@qq.com>
2017-11-11 18:37:18 +0800

    改用druid-spring-boot-starter

--- springboot-druid/src/main/java/com/louie/learning/springboot/configuration/DruidDataSourceConfig.java
+++ springboot-druid/src/main/java/com/louie/learning/springboot/configuration/DruidDataSourceConfig.java
@@ -1,32 +1,32 @@
-package com.louie.learning.springboot.configuration;
-
-import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
-
-/**
- * Created by Administrator on 2017/9/7.
- */
-@Configuration
-@ConditionalOnClass({DruidDataSource.class})
-@ConditionalOnProperty(
-        name = {"spring.datasource.type"},
-        havingValue = "com.alibaba.druid.pool.DruidDataSource",
-        matchIfMissing = true
-)
-public class DruidDataSourceConfig {
-
-    @Bean(name = "dataSource", initMethod = "init", destroyMethod = "close")
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource druidDataSource() {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        return druidDataSource;
-    }
-}
+//package com.louie.learning.springboot.configuration;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import javax.sql.DataSource;
+//
+///**
+// * Created by Administrator on 2017/9/7.
+// */
+//@Configuration
+//@ConditionalOnClass({DruidDataSource.class})
+//@ConditionalOnProperty(
+//        name = {"spring.datasource.type"},
+//        havingValue = "com.alibaba.druid.pool.DruidDataSource",
+//        matchIfMissing = true
+//)
+//public class DruidDataSourceConfig {
+//
+//    @Bean(name = "dataSource", initMethod = "init", destroyMethod = "close")
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource druidDataSource() {
+//        DruidDataSource druidDataSource = new DruidDataSource();
+//        return druidDataSource;
+//    }
+//}
--- springboot-druid/src/main/java/com/louie/learning/springboot/configuration/DruidStatConfig.java
+++ springboot-druid/src/main/java/com/louie/learning/springboot/configuration/DruidStatConfig.java
@@ -1,54 +1,54 @@
-package com.louie.learning.springboot.configuration;
-
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
-
-
-@Configuration
-public class DruidStatConfig {
-
-    /**
-     * 注册一个StatViewServlet
-     *
-     * @return
-     */
-    @Bean
-    public ServletRegistrationBean druidStatViewServle() {
-        // org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
-//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
-//        servletRegistrationBean.setServlet(new StatViewServlet());
-//        servletRegistrationBean.addUrlMappings("/druid/*");
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-        // 添加初始化参数：initParams
-        servletRegistrationBean.addInitParameter("loginUsername", "admin");// 用户名
-        servletRegistrationBean.addInitParameter("loginPassword", "admin");// 密码
-        servletRegistrationBean.addInitParameter("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
-        servletRegistrationBean.addInitParameter("allow", "127.0.0.1"); // IP白名单 (没有配置或者为空，则允许所有访问)
-        servletRegistrationBean.addInitParameter("deny", "192.168.20.38");// IP黑名单(存在共同时，deny优先于allow)
-        return servletRegistrationBean;
-    }
-
-    /**
-     * druid过滤器
-     *
-     * @return
-     */
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-//        filterRegistrationBean.setFilter(new WebStatFilter());
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        // 添加过滤规则.
-        filterRegistrationBean.addUrlPatterns("/*");
-        // 添加不需要忽略的格式信息.
-        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
-        return filterRegistrationBean;
-    }
-}
+//package com.louie.learning.springboot.configuration;
+//
+//import com.alibaba.druid.support.http.StatViewServlet;
+//import com.alibaba.druid.support.http.WebStatFilter;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//
+//@Configuration
+//public class DruidStatConfig {
+//
+//    /**
+//     * 注册一个StatViewServlet
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public ServletRegistrationBean druidStatViewServle() {
+//        // org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+////        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+////        servletRegistrationBean.setServlet(new StatViewServlet());
+////        servletRegistrationBean.addUrlMappings("/druid/*");
+//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+//        // 添加初始化参数：initParams
+//        servletRegistrationBean.addInitParameter("loginUsername", "admin");// 用户名
+//        servletRegistrationBean.addInitParameter("loginPassword", "admin");// 密码
+//        servletRegistrationBean.addInitParameter("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
+//        servletRegistrationBean.addInitParameter("allow", "127.0.0.1"); // IP白名单 (没有配置或者为空，则允许所有访问)
+//        servletRegistrationBean.addInitParameter("deny", "192.168.20.38");// IP黑名单(存在共同时，deny优先于allow)
+//        return servletRegistrationBean;
+//    }
+//
+//    /**
+//     * druid过滤器
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public FilterRegistrationBean filterRegistrationBean() {
+////        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+////        filterRegistrationBean.setFilter(new WebStatFilter());
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+//        // 添加过滤规则.
+//        filterRegistrationBean.addUrlPatterns("/*");
+//        // 添加不需要忽略的格式信息.
+//        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
+//        return filterRegistrationBean;
+//    }
+//}
